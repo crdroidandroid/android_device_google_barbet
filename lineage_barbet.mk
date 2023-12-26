402af88d6ec67b6febb84480e707a19190ee6743
Bobby306 <143777624+Bobby306@users.noreply.github.com>
2023-12-25 19:25:15 -0500

    Disable artifact path reqs

--- lineage_barbet.mk
+++ lineage_barbet.mk
@@ -5,6 +5,7 @@
 #
 
 # Inherit some common Lineage stuff.
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
