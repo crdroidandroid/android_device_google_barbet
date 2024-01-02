2580eecff4d2dafa26a9718beecbaa42f01f6ba6
Bobby306 <143777624+Bobby306@users.noreply.github.com>
2024-01-01 19:04:25 -0500

    barbet: remove hbmsvmanager

--- device-lineage.mk
+++ device-lineage.mk
@@ -6,9 +6,4 @@
 
 $(call inherit-product, device/google/redbull/device-lineage.mk)
 
-# CHRE
-$(call soong_config_set,chre,chre_daemon_dsp_library,//vendor/google/barbet:libadsprpc)
 
-# HBM
-PRODUCT_PACKAGES += \
-    HbmSVManagerOverlay
